ecac3bd9ff63b79729ee87bc4333e32027a9af9f
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2025-01-20 21:01:08 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    let menu = document.getElementById('leftMenu');
+    menu.style.width = menu.style.width === "50px" ? "250px" : "50px";
+}
+
+function adjustPageSize() {
+    let width = window.innerWidth;
+    let scale = 1;
+    if (width >= 992 && width <= 1600) {
+        scale = 0.9;
+    } else if (width >= 700 && width <= 767) {
+        scale = 0.8;
+    } else if (width >= 600 && width < 700) {
+        scale = 0.75;
+    } else if (width <= 600) {
+        scale = 0.5;
+    }
+    document.body.style.transform = `scale(${scale})`;
+    document.body.style.transformOrigin = 'top left';
+}
+
+window.addEventListener("resize", adjustPageSize);
+window.addEventListener("load", adjustPageSize);
